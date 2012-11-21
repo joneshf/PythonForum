8ca358beb2adcbee311e00cc8f20f8346d16c70e
Jakob Bowyer <jkbbwr@gmail.com>
2012-11-21 22:06:36 +0000

    Made logfiles rotate

--- deploy.py
+++ deploy.py
@@ -1,8 +1,15 @@
 from twisted.internet import reactor
 from twisted.web.wsgi import WSGIResource
 from twisted.web.server import Site
+from twisted.python import log
+from twisted.python.logfile import DailyLogFile
+
+from sys import stdout
 
 from PythonForum import app
+
+log.startLogging(stdout)
+log.startLogging(DailyLogFile("python-forum.log", "."))
 
 resource = WSGIResource(reactor, reactor.getThreadPool(), app)
 site = Site(resource)
